e590d943be16ef3354500a52ef30bbd58cf1d431
Maxim Vainshtein <maximv88@gmail.com>
2016-06-03 15:31:28 +0300

    Initial Commit

---
+++ Neural/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Neural
+//
+//  Created by Maxim Vainshtein on 03/06/2016.
+//  Copyright © 2016 Maxim Vainshtein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
